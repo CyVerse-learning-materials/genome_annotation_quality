3b5160c6dcb134caebd14be67c33b0525a8ecc6c
ritututeja <reetututeja@Cyverses-MacBook-Pro.local>
2019-12-19 11:21:29 -0700

    modified conf

--- conf.py
+++ conf.py
@@ -9,9 +9,9 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 from misc.cyverse_sphinx_conf import *  # noqa
 
-project = 'Git for Mere Mortals as topic_tutorial'
+project = 'Evaluate Genome Annotation Quality'
 copyright = '2019, CyVerse'
-author = 'Meghan Balk', 'Ramona L. Walls'
+author = 'Reetu Tuteja'
 version = '1.0'
 release = '1.0'
 
